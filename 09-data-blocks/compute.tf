32f0dfe1329a936873388b1b6ca90f92a0b697b5
wasadmin <wasadmin@ip-172-31-69-189.ec2.internal>
2023-09-19 14:15:15 +0000

    data-blocks

---
+++ 09-data-blocks/compute.tf
@@ -0,0 +1,31 @@
+// Retrieve the most recent Amazon Linux AMI 
+
+# data block to retrieve latest Amazon Linux AMI
+data "aws_ami" "latest_amz_linux_oregon" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn*"]
+  }
+}
+
+
+output "amidid" {
+  value = data.aws_ami.latest_amz_linux_oregon.id
+}
+
+
+# Create an AWS instance with the retrieved AMI and instance type
+resource "aws_instance" "amz-linux" {
+  ami           = data.aws_ami.latest_amz_linux_oregon.id
+  instance_type = "t3.nano"
+
+  # Tag the instance with a fictitious name
+  tags = {
+    Name = "Infra-Amazon-Linux-Latest"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.amz-linux.id
+}
