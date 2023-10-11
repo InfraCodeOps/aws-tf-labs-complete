b06e5142d937fceb17f3fac8ae26e1963327e268
jazzwall <jazzwall@gmail.com>
2023-10-11 16:36:20 -0500

    updated provider and AMI query in lab 4

--- 04-aws-resources/compute.tf
+++ 04-aws-resources/compute.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "lab4vm" {
+
+  # retrieve the AMI using the following bash command:
+  # aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.*-gp2' 'Name=architecture,Values=x86_64' 'Name=root-device-type,Values=ebs' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --region us-west-2
   # your AMI might be different
-  ami           = "ami-0c2caccf78f4fa033"
+  ami           = "ami-0044a0897b53acfb6"
   instance_type = "t3.nano"
 
   // Tag the instance with a fictitious name
